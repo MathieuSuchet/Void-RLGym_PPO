729723a91c67df5e148f1e923fe44a9ff2385488
Mathieu Suchet <mathieu.suchet@ecole-hexagone.com>
2024-05-10 23:09:34 +0200

    new live instance

--- live_instance.py
+++ live_instance.py
@@ -1,18 +1,28 @@
+# region ========================= Imports =========================
+import os
+import random
+import time
+
 import numpy as np
 import torch
-from rlgym import make
 from rlgym.gamelaunch import LaunchPreference
 from rlgym_ppo.ppo import PPOLearner
-from rlgym_sim.utils.reward_functions.common_rewards import ConstantReward
+from rlgym_sim.utils.reward_functions import CombinedReward
+from rlgym_sim.utils.reward_functions.common_rewards import EventReward
 from rlgym_sim.utils.terminal_conditions.common_conditions import TimeoutCondition, GoalScoredCondition
 from rlgym_tools.extra_obs.advanced_padder import AdvancedObsPadder
 
 from rlgym1_assets.action_parsers.action_parsers import WandbActionParser, LookupAction
-from rlgym1_assets.rewards.rewards import LoggerCombinedReward
+from rlgym1_assets.rewards.NewRewards import PlayerVelocityReward, KickoffReward_MMR, SaveBoostReward, \
+    BoostPickupReward, DribbleReward
+from rlgym1_assets.rewards.rewards import BumpReward, GoalScoreSpeed
 from rlgym1_assets.state_mutators.state_mutators import DefaultState, TeamSizeSetter, \
     DynamicScoredReplaySetter
 from rlgym1_assets.terminal_conditions.multi_condition import MultiLoggedCondition
+from utils import get_latest_model_path, live_log
 
+# endregion
+# region ========================= Environment settings ============================
 tick_skip = 8
 STEP_TIME = tick_skip / 120.
 
@@ -25,18 +35,43 @@ dynamic_replay = DynamicScoredReplaySetter(
     "replays/states_scores_doubles.npz",
     "replays/states_scores_standard.npz"
 )
-dynamic_replay.load_replays()
+# dynamic_replay.load_replays()
 state_mutator = TeamSizeSetter(
     setters=(
         DefaultState(),
-        dynamic_replay
+        # dynamic_replay
     ),
-    weights=(1, 1),
+    weights=(1,),
     gm_probs=(0.3, 0.4, 0.3)
 )
-action_parser = WandbActionParser(LookupAction())
-obs_builder = AdvancedObsPadder()
-reward_fn = ConstantReward()
+
+rewards_functions = (
+    EventReward(
+        goal=1,
+        team_goal=1.2,
+        concede=-1,
+        touch=0.75,
+        shot=0.6,
+        save=0.7,
+        demo=0.8,
+        boost_pickup=0.5
+    ),
+    BumpReward(),
+    PlayerVelocityReward(),
+    GoalScoreSpeed(),
+    KickoffReward_MMR(),
+    SaveBoostReward(),
+    BoostPickupReward(),
+    DribbleReward()
+)
+
+rewards_weights = (
+    0.5, 0.01, 0.0007, 30, 20, 0.0005, 0.01, 0.01
+)
+reward_fn = CombinedReward(
+    reward_functions=rewards_functions,
+    reward_weights=rewards_weights
+)
 
 total_timeout = 20
 termination_conditions = [MultiLoggedCondition(
@@ -44,58 +79,132 @@ termination_conditions = [MultiLoggedCondition(
 
     TimeoutCondition(int(total_timeout / STEP_TIME)),
 )]
+# endregion
+# region ========================= Model Settings =============================
+
+action_parser = WandbActionParser(LookupAction())
+obs_builder = AdvancedObsPadder()
+
+agent = PPOLearner(
+    obs_space_size=237,
+    act_space_size=action_parser.get_action_space().n,
+    device="cuda",
+    batch_size=10_000,
+    mini_batch_size=1_000,
+    n_epochs=10,
+    continuous_var_range=(0.0, 1.0),
+    policy_type=0,
+    policy_layer_sizes=(256, 256, 256),
+    critic_layer_sizes=(256, 256, 256),
+    policy_lr=3e-4,
+    critic_lr=3e-4,
+    clip_range=0.2,
+    ent_coef=0.005)
+
+# endregion
+# region ========================= Live instance Settings =============================
 deterministic = True
 
-model_to_load = "data/rl_model/-1714578008627305700/152007804"
+model_to_load = get_latest_model_path("data/rl_model")
 
+minutes_before_update = 15
+seconds_before_update = 0
+time_before_update = minutes_before_update * 60 + seconds_before_update
+current_time = 0
+last_ep_reward = 0
 
-def create_env():
-    rlgym_env = make(
-        state_setter=state_mutator,
-        action_parser=action_parser,
-        obs_builder=obs_builder,
-        reward_fn=reward_fn,
-        terminal_conditions=termination_conditions,
-        tick_skip=tick_skip,
-        team_size=blue_count,
-        spawn_opponents=spawn_opponents,
-        game_speed=1,
-        launch_preference=LaunchPreference.EPIC
-    )
+# endregion
+# region ========================= Live instance functions =========================
+
+def create_env(sim: bool = True):
+    if sim:
+        import rlgym_sim
+        rlgym_env = rlgym_sim.make(
+            state_setter=state_mutator,
+            action_parser=action_parser,
+            obs_builder=obs_builder,
+            reward_fn=reward_fn,
+            terminal_conditions=termination_conditions,
+            tick_skip=tick_skip,
+            team_size=blue_count,
+            spawn_opponents=spawn_opponents,
+            # game_speed=1,
+            # launch_preference=LaunchPreference.EPIC
+        )
+    else:
+        import rlgym
+        rlgym_env = rlgym.make(
+            state_setter=state_mutator,
+            action_parser=action_parser,
+            obs_builder=obs_builder,
+            reward_fn=reward_fn,
+            terminal_conditions=termination_conditions,
+            tick_skip=tick_skip,
+            team_size=blue_count,
+            spawn_opponents=spawn_opponents,
+            game_speed=1,
+            launch_preference=LaunchPreference.EPIC
+        )
 
     return rlgym_env
 
 
-if __name__ == "__main__":
+def model_reload():
+    global agent, current_time
+    latest_model_path = get_latest_model_path("data/rl_model")
+    agent.load_from(latest_model_path)
+    live_log("Model reloaded")
+    current_time = time.time()
 
-    agent = PPOLearner(
-        obs_space_size=237,
-        act_space_size=action_parser.get_action_space().n,
-        device="cuda",
-        batch_size=10_000,
-        mini_batch_size=1_000,
-        n_epochs=10,
-        continuous_var_range=(0.0, 1.0),
-        policy_type=0,
-        policy_layer_sizes=(256, 256, 256),
-        critic_layer_sizes=(256, 256, 256),
-        policy_lr=3e-4,
-        critic_lr=3e-4,
-        clip_range=0.2,
-        ent_coef=0.005)
+
+def playstyle_switch():
+    global deterministic
+
+    deterministic = random.uniform(0, 1) > 0.5
+
+
+def print_live_state():
+    ttu = time_before_update - (time.time() - current_time)
+    os.system('cls')
+    live_log(" === State report === ")
+    live_log(f" Mode : {'Deterministic' if deterministic else 'Stochastic'}")
+    live_log(f" Model reload in {int(ttu // 60):02d}:{int(ttu % 60):02d}")
+    live_log(f" Last episode reward (Average per player) : {last_ep_reward:.6f}")
+
+# endregion
+# region ========================= Main loop =========================
+
+if __name__ == "__main__":
 
     agent.load_from(model_to_load)
 
-    env = create_env()
+    env = create_env(sim=True)
+    current_time = time.time()
+    refresh_time = current_time
+    rewards = []
 
     while True:
+        playstyle_switch()
         obs = env.reset()
-        while True:
+        if len(rewards) > 0:
+            last_ep_reward = sum(rewards) / len(rewards)
+        rewards = []
+        terminated = False
+        while not terminated:
+            if time.time() - refresh_time >= 1:
+                refresh_time = time.time()
+                print_live_state()
+
             with torch.no_grad():
                 actions = np.array([agent.policy.get_action(o, deterministic)[0] for o in obs])
                 actions = actions.reshape((*actions.shape, 1))
 
             obs, reward, terminated, info = env.step(actions)
+            rewards.append(sum(reward) / len(reward))
 
-            if terminated:
-                obs = env.reset()
+            if time.time() - current_time >= time_before_update:
+                model_reload()
+
+            time.sleep(STEP_TIME)
+
+# endregion
--- utils.py
+++ utils.py
@@ -6,6 +6,12 @@ def _predicate(path):
 
 
 def get_latest_model_path(checkpoints_path: str) -> str:
-    latest_run = sorted(os.listdir(checkpoints_path), key=lambda y: _predicate(os.path.join(checkpoints_path, y)), reverse=True)[0]
-    latest_model = sorted(os.listdir(os.path.join(checkpoints_path, latest_run)), key=lambda y: _predicate(os.path.join(checkpoints_path, latest_run, y)), reverse=True)[0]
+    latest_run = \
+    sorted(os.listdir(checkpoints_path), key=lambda y: _predicate(os.path.join(checkpoints_path, y)), reverse=True)[0]
+    latest_model = sorted(os.listdir(os.path.join(checkpoints_path, latest_run)),
+                          key=lambda y: _predicate(os.path.join(checkpoints_path, latest_run, y)), reverse=True)[0]
     return os.path.join(checkpoints_path, latest_run, latest_model)
+
+
+def live_log(*content):
+    print("[LIVE INSTANCE] -", *content)
